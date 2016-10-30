29efea1eb4252d50f7ad80386dc3b112fbbfe967
Rudnev Ivan <Rudnev Ivan>
2016-10-30 19:52:49 +0300

    Added database helper

---
+++ app/src/main/java/com/rv150/bestbefore/Services/DBHelper.java
@@ -0,0 +1,55 @@
+package com.rv150.bestbefore.Services;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Rudnev on 30.10.2016.
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+    
+    public static class AutoCompletedProducts implements BaseColumns {
+        public static final String TABLE_NAME = "user_products";
+        public static final String COLUMN_NAME_NAME = "name";
+    }
+
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 2;
+    public static final String DATABASE_NAME = "BestBefore.db";
+
+    private static final String DEFAULT_CHARSET =
+            "DEFAULT CHARSET utf8 DEFAULT COLLATE utf8_general_ci";
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + AutoCompletedProducts.TABLE_NAME + " (" +
+                    AutoCompletedProducts._ID + " INTEGER PRIMARY KEY," +
+                    AutoCompletedProducts.COLUMN_NAME_NAME + " VARCHAR(100) UNIQUE)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + AutoCompletedProducts.TABLE_NAME;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
+
