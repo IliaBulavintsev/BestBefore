88ba4f144de84923306378024be819f7800a5263
Ivan Rudnev <rudnev.vanya@gmail.com>
2017-01-24 12:12:53 +0300

    Quantity limit

--- app/src/main/java/com/rv150/bestbefore/Activities/MainActivity.java
+++ app/src/main/java/com/rv150/bestbefore/Activities/MainActivity.java
@@ -852,7 +852,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 doubleBackToExitPressedOnce=false;
             }
-        }, 1000);
+        }, 2000);
     }
 
     public void finishAct() {
