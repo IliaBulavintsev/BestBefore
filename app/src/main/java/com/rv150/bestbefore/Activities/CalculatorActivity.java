b0ab1c0962f99d592a2e8c86d9ad9050a6a86759
Ivan Rudnev <rudnev.vanya@gmail.com>
2017-04-27 22:36:34 +0300

    Other views

--- app/src/main/java/com/rv150/bestbefore/Activities/CalculatorActivity.java
+++ app/src/main/java/com/rv150/bestbefore/Activities/CalculatorActivity.java
@@ -2,10 +2,16 @@ package com.rv150.bestbefore.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.rv150.bestbefore.R;
+
+import java.util.Calendar;
 
 /**
  * Created by ivan on 27.04.17.
@@ -17,5 +23,28 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        setTitle(R.string.calculator);
+
+        Calendar now = Calendar.getInstance();
+
+        final TextView result = (TextView) findViewById(R.id.result);
+
+        DatePicker datePicker = (DatePicker) findViewById(R.id.date);
+        datePicker.init(now.get(Calendar.YEAR), now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
+                    @Override
+                    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        result.setText(String.valueOf(year + monthOfYear + dayOfMonth));
+                    }
+                });
+
+        Spinner spinnerStorageLife = (Spinner)findViewById(R.id.spinner_storage_life);
+        String[] terms = new String[]{
+                getString(R.string.days_in_add_act),
+                getString(R.string.months_in_add_act)};
+        ArrayAdapter<String> spinnerAdapter =
+                new ArrayAdapter<>(this, R.layout.custom_xml_spinner_layout, terms);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerStorageLife.setAdapter(spinnerAdapter);
     }
 }
